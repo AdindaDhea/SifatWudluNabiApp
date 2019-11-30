05712ea3befe8e8463d84fb4030d65327e7ccfd9
RizkiTrisna <rzkid.ra@gmail.com>
2019-11-30 07:03:09 +0700

    Upload awal

---
+++ app/src/main/java/com/example/gotakecoffee_2/user_detailMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.gotakecoffee_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_detailMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detail_menu);
+    }
+}
